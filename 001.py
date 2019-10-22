0d52770f3615f8e58d3fbc8a4e7ab2e0fdfd6fed
John Santerre <john.santerre@gmail.com>
2019-10-22 13:07:50 -0700

    Update 001.py

--- 001.py
+++ 001.py
@@ -1,4 +1,27 @@
 # Prepare my_utils.py file
 
-# download data from csv
+# download data from project gutenberg
+import urllib.request
+request = urllib.request.Request('https://www.gutenberg.org/files/219/219-0.txt')
+response = urllib.request.urlopen(request)
+data = response.read().decode('utf-8')
+
+
+# String Manipulation functions
+
+# Lower case
+# Remove punctuation
+# Wrap lines
+
+# What words follow what words?
+
+# What words co-occur in the same sentence?
+
+# Represnt as bag of words?
+
+
+
+
+# Who are the main Characters?
+
 
